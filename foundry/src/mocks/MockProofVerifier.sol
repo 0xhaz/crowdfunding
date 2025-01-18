1e032877a80b45ccb7233bd9a823ff29515c1302
0xhaz <28639279+0xhaz@users.noreply.github.com>
2025-01-19 00:29:36 +0800

    Added Test

--- foundry/src/mocks/MockProofVerifier.sol
+++ foundry/src/mocks/MockProofVerifier.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.27;
 import {IStarkVerifier, ProofVerifier} from "src/circuitZK/ProofVerifier.sol";
 
 contract MockProofVerifier is IStarkVerifier {
-    function verifyStarkProof(bytes calldata proof, bytes32 publicInputs) external pure returns (bool) {
+    function verifyStarkProof(bytes calldata, /*proof*/ bytes32 /*publicInputs*/ ) external pure returns (bool) {
         return true;
     }
 }
